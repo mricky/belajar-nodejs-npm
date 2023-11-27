3add0008d408d5ececfea97a4eb5dcc3212d3be6
Mohammad <mricky.it@gmail.com>
2023-11-27 11:38:42 +0700

    management library nodejs

---
+++ dependecy.js
@@ -0,0 +1,7 @@
+import _ from "lodash";
+
+
+const soure  = "MOHAMMAD RICKY SUSANTO";
+const target = _.capitalize(soure);
+
+console.log(target);
---
+++ hello.js
@@ -0,0 +1,6 @@
+import { sayHello, sum } from "mricky-belajar-nodejs-library";
+
+console.info(sayHello("Mohammad Ricky"));
+
+const numbers = [10,10,10,10];
+console.info(sum(numbers));
